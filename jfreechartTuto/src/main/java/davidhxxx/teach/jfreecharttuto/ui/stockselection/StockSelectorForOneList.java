57661cffc63d61eb3cf2838bbff903ab06193aea
davidou <davidou@davidou-PC>
2015-12-13 13:15:20 +0100

    code unused

--- jfreechartTuto/src/main/java/davidhxxx/teach/jfreecharttuto/ui/stockselection/StockSelectorForOneList.java
+++ jfreechartTuto/src/main/java/davidhxxx/teach/jfreecharttuto/ui/stockselection/StockSelectorForOneList.java
@@ -3,7 +3,6 @@ package davidhxxx.teach.jfreecharttuto.ui.stockselection;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
