834220aa5cd8f26aae28f7c0117175f16999d234
davidou <davidou@davidou-PC>
2016-05-29 11:34:59 +0200

    in progress

--- jfreechartTuto/src/main/java/davidhxxx/teach/jfreecharttuto/util/ApplicationDirs.java
+++ jfreechartTuto/src/main/java/davidhxxx/teach/jfreecharttuto/util/ApplicationDirs.java
@@ -11,6 +11,7 @@ import org.supercsv.prefs.CsvPreference;
 
 public class ApplicationDirs {
 
+
     static {
 
 	Configuration config = null;
@@ -18,21 +19,21 @@ public class ApplicationDirs {
 	    config = new PropertiesConfiguration("config.properties");
 	}
 	catch (ConfigurationException e) {
-	    throw new IllegalArgumentException("Erreur lors du chargement du fichier de configuration");
+	    throw new IllegalArgumentException("Error during configuration file loading");
 	}
 
 	String appBaseDir = config.getString("app.base.dir");
 	if (StringUtils.isEmpty(appBaseDir)) {
-	    throw new IllegalArgumentException("la propriété doit être renseignée dans le fichier config.properties");
+	    throw new IllegalArgumentException("app.base.dir property must be valued in config.properties");
 	}
 
 	File repertoireBase = new File(appBaseDir);
 	if (!repertoireBase.exists()) {
-	    throw new IllegalArgumentException("le repértoire " + repertoireBase + " n'existe pas");
+	    throw new IllegalArgumentException("directory " + repertoireBase + " doesn't exist");
 	}
 
 	if (!repertoireBase.isDirectory()) {
-	    throw new IllegalArgumentException("le chemin " + repertoireBase + " n'est pas un répertoire");
+	    throw new IllegalArgumentException("the path " + repertoireBase + " is not a directory");
 	}
 
 	BASE_DIR = appBaseDir;
