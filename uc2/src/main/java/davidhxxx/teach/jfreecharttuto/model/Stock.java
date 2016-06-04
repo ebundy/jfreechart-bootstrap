329fbce3f7c164c92573ba494ddbde90285167bd
ebundy <ebundy@gmail.com>
2016-06-04 15:05:53 +0200

    dd

---
+++ uc2/src/main/java/davidhxxx/teach/jfreecharttuto/model/Stock.java
@@ -0,0 +1,24 @@
+package davidhxxx.teach.jfreecharttuto.model;
+
+public class Stock {
+
+    private String isin;
+
+    private String name;
+
+    private String ticker;
+
+    public String getIsin() {
+	return isin;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public String getTicker() {
+	return ticker;
+    }
+
+
+}
